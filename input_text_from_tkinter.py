a39644959e640cfe16c61aac1c79826c398b6151
Bryan0066 <bgenest96@gmail.com>
2022-03-29 23:37:48 -0400

    Create input_text_from_tkinter.py

---
+++ input_text_from_tkinter.py
@@ -0,0 +1,18 @@
+##input_text_from_tkinter.py
+
+
+
+import tkinter as tk
+
+
+"""
+with open('readme.txt', 'w') as f:
+    f.write('readme')
+"""
+
+
+##pop up window with text box 
+
+##adding a button to the window, make the button add the text input add to a text file
+
+##after writing to the file, the text in the box is deleted. Window stays up,
